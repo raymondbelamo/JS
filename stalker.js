1687ad95170dd146e72bffd660262c34b9688324
raymondbelamo <60197398+raymondbelamo@users.noreply.github.com>
2020-09-28 19:21:46 +0100

    Create stalker.js

---
+++ stalker.js
@@ -0,0 +1,10 @@
+<script>
+
+let firstName = prompt("What's your first name?");
+let lastName = prompt("What's your last name?");
+let age = prompt("What's your age?");
+
+let info = alert(`Your full name is ${firstName} ${lastName} and you're ${age} years old`);
+console.log(info);
+
+</script>
