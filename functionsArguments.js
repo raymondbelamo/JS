0266f1ddc7b6ae86417a6f4013741c1e89726dbf
raymondbelamo <60197398+raymondbelamo@users.noreply.github.com>
2020-11-10 20:12:38 +0100

    Update functionsArguments.js

--- functionsArguments.js
+++ functionsArguments.js
@@ -37,5 +37,14 @@ function isShortsWeather (temperature) {
         return true;
     }
 }
+
+//Another function
+function lastElement(arr) {
+    if (!arr.length) return null;
+    return arr[arr.length - 1];
+}
+console.log(lastElement([3, 5, 7])); 
+console.log(lastElement([1]));
+console.log(lastElement([])); 
 </script>
 
